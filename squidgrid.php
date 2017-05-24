41efb8666dce2432695733e48a38749c3092c47d
Ricardo <ricardo-verdugo@hotmail.com>
2017-05-24 17:59:53 +0100

    gitkeep

--- squidgrid.php
+++ squidgrid.php
@@ -2,6 +2,7 @@
 namespace Grav\Plugin;
 
 use Grav\Common\Plugin;
+use Grav\Common\Page;
 use RocketTheme\Toolbox\Event\Event;
 
 /**
@@ -24,10 +25,59 @@ class SquidGridPlugin extends Plugin
     {
         return [
             'onPluginsInitialized' => ['onPluginsInitialized', 0],
-            'onGetPageBlueprints'  => ['onGetPageBlueprints', 0]
+            'onGetPageBlueprints'  => ['onGetPageBlueprints', 0],
+            'onPageContentRaw'  => ['onPageContentRaw', 0]
         ];
     }
 
+    private function getCode()
+    {
+
+        "$(document).ready(function() {
+            $('#{{ unique_id }}').mediaBoxes({
+                boxesToLoadStart: {{ settings.boxesToLoadStart|default(8) }},
+                boxesToLoad: {{ settings.boxesToLoad|default(4) }},
+                lazyLoad: {{ settings.lazyLoad|default('true') }},
+                horizontalSpaceBetweenBoxes: {{ settings.horizontalSpaceBetweenBoxes|default(15) }},
+                verticalSpaceBetweenBoxes: {{ settings.verticalSpaceBetweenBoxes|default(15) }},
+                columnWidth: '{{ settings.columnWidth|default('auto') }}',
+                columns: {{ settings.columns|default(4) }},
+                multipleFilterLogic: '{{ settings.multipleFilterLogic|default('AND') }}',
+                filterContainer: '{{ settings.filterContainer|default('#filter') }}',
+                search: '{{ settings.search|default('.media-boxes-search') }}',
+                searchTarget: '{{ settings.searchTarget|default('.media-box-content') }}',
+                sortContainer: '{{ settings.sortContainer|default('') }}',
+                minBoxesPerFilter: {{ settings.minBoxesPerFilter|default(0) }},
+                waitUntilThumbWithRatioLoads: {{ settings.waitUntilThumbWithRatioLoads|default('true') }},
+                waitForAllThumbsNoMatterWhat: {{ settings.waitForAllThumbsNoMatterWhat|default('false') }},
+                thumbnailOverlay: {{ settings.thumbnailOverlay|default('true') }},
+                overlayEffect: '{{ settings.overlayEffect|default('fade') }}',
+                overlaySpeed: {{ settings.overlaySpeed|default(200) }},
+                overlayEasing: '{{ settings.overlayEasing|default('default') }}',
+                showOnlyLoadedBoxesInPopup: {{ settings.showOnlyLoadedBoxesInPopup|default('false') }},
+                considerFilteringInPopup: {{ settings.considerFilteringInPopup|default('true') }},
+                deepLinkingOnPopup: {{ settings.deepLinkingOnPopup|default('true') }},
+                deepLinkingOnFilter: {{ settings.deepLinkingOnFilter|default('true') }},
+                deepLinkingOnSearch: {{ settings.deepLinkingOnSearch|default('false') }},
+                gallery: {{ settings.gallery|default('true') }},
+                LoadingWord: '{{ settings.enableDrag|default('Loading...') }}',
+                loadMoreWord: '{{ settings.enableDrag|default('Load More') }}',
+                noMoreEntriesWord: '{{ settings.enableDrag|default('No More Entries') }}'
+            });
+        });";
+
+
+    }
+
+    public function onPageContentRaw()
+    {
+        if (isset($this->grav['page']->header()->squidgrid)) {
+//            $squidgrid = $this->grav['page']->header()->squidgrid[config];
+        }else {
+//            $squidgrid = $this->config->get('plugins.squidgrid');
+        }
+//        dump($squidgrid);
+    }
     /**
      * Add page blueprints
      *
